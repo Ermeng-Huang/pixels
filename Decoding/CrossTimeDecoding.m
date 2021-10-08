4df00c29ef0a80b0de3a8b271ea5ebca67249dff
Ermeng-Huang <81205333+Ermeng-Huang@users.noreply.github.com>
2021-10-07 23:24:14 -0700

    Add files via upload

--- Decoding/CrossTimeDecoding.m
+++ Decoding/CrossTimeDecoding.m
@@ -16,76 +16,73 @@ rpts=size(decdata.train.s1,4);
 cv=cvpartition(trlN,'KFold',10);
 y=[zeros(trlN,1);ones(trlN,1)];
 
-out=struct();
-out.cvcorr=cell(bins,bins);
-out.shufcorr=cell(bins,bins);
-%     errcorr{bin+3}=[];
-
-cv_trainingID=cell2mat(arrayfun(@(x)training(cv,x),[1:10],'UniformOutput',false));
-cv_testID=cell2mat(arrayfun(@(x)test(cv,x),[1:10],'UniformOutput',false));
-
-for rpt=1:rpts
+% parpool(4)
+parfor rpt=1:rpts
+    cvresult=cell(bins,bins);
+    cvshufresult=cell(bins,bins);
+    cv_trainingID=cell2mat(arrayfun(@(x)training(cv,x),1:10,'UniformOutput',false));
+    cv_testID=cell2mat(arrayfun(@(x)test(cv,x),1:10,'UniformOutput',false));
+    
     fprintf('rpts %d of %d\n',rpt,rpts);
-    for kf=1:cv.NumTestSets
-        for bin=1:bins
-            for bin2=1:bins            
-                s1kf=decdata.train.s1(:,cv_trainingID(:,kf),bin,rpt);
-                s2kf=decdata.train.s2(:,cv_trainingID(:,kf),bin,rpt);
-                varsel=any(s1kf-s1kf(:,1),2) & any(s2kf-s2kf(:,1),2);
-                s1kf=s1kf(varsel,:);
-                s2kf=s2kf(varsel,:);
-                %             Xerr=[s1err(:,varsel);s2err(:,varsel)];
-                %             yerr=[zeros(size(s1err,1),1);ones(size(s2err,1),1)];
-                Xkf=cat(2,s1kf,s2kf)';
-                ykf=y([cv_trainingID(:,kf);cv_trainingID(:,kf)]);
-                
+    for bin=1:bins       
+        for kf=1:cv.NumTestSets
+            s1kf=decdata.train.s1(:,cv_trainingID(:,kf),bin,rpt);
+            s2kf=decdata.train.s2(:,cv_trainingID(:,kf),bin,rpt);
+            varsel=any(s1kf-s1kf(:,1),2) & any(s2kf-s2kf(:,1),2);
+            s1kf=s1kf(varsel,:);
+            s2kf=s2kf(varsel,:);
+            %             Xerr=[s1err(:,varsel);s2err(:,varsel)];
+            %             yerr=[zeros(size(s1err,1),1);ones(size(s2err,1),1)];
+            Xkf=cat(2,s1kf,s2kf)';
+            ykf=y([cv_trainingID(:,kf);cv_trainingID(:,kf)]);        
+            yshufkf=ykf(randperm(numel(ykf)));
+            if strcmp(opt.decoder,'SVM')
+                SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',true);         
+                SVMMshuf=fitcsvm(Xkf,yshufkf,'KernelFunction','linear','Standardize',true); 
+            elseif strcmp(opt.decoder,'LDA')
+                LDAM=fitcdiscr(Xkf,ykf);               
+            elseif strcmp(opt.decoder,'NB')
+                NBM=fitcnb(Xkf,ykf);
+            end
+            for bin2=1:bins
                 s1Tkf=decdata.test.s1(varsel,cv_testID(:,kf),bin2,rpt);
                 s2Tkf=decdata.test.s2(varsel,cv_testID(:,kf),bin2,rpt);
                 XTkf=cat(2,s1Tkf,s2Tkf)';
-                yTkf=y([cv_testID(:,kf);cv_testID(:,kf)]);
+                yTkf=y([cv_testID(:,kf);cv_testID(:,kf)]);                
+                                           
+                cvresult{bin,bin2}=SVMM.predict(XTkf)==yTkf;
+                cvshufresult{bin,bin2}=SVMMshuf.predict(XTkf)==yTkf;
                 
-                Train{bin,bin2}=[Xkf,ykf];
-                Test{bin,bin2}=[XTkf,yTkf];
-            end
-        end  
-        
-        if strcmp(opt.decoder,'SVM')
-            SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',true);
-            modelPredict=SVMM.predict(XTkf);
-        elseif strcmp(opt.decoder,'LDA')
-            LDAM=fitcdiscr(Xkf,ykf);
-            modelPredict=LDAM.predict(XTkf);
-        elseif strcmp(opt.decoder,'NB')
-            NBM=fitcnb(Xkf,ykf);
-            modelPredict=NBM.predict(XTkf);
-        end
+               
                 
-                cvresult=modelPredict==yTkf;
-                out.cvcorr{bin,bin2}=cat(1,out.cvcorr{bin,bin2},cvresult);
-                
-                for i=1:1000
-                    yshufTkf=yTkf(randperm(numel(yTkf)));
-                    cvshufresult=modelPredict==yshufTkf;
-                    out.shufcorr{bin,bin2}=cat(1,out.shufcorr{bin,bin2},cvshufresult);
-                end
+%                 for i=1:50
+%                     yshufTkf=yTkf(randperm(numel(yTkf)));
+%                     cvshufresult=modelPredict==yshufTkf;
+%                     shufcorr{rpt}{bin,bin2}=cvshufresult;
+%                 end
                 
                 %             cv_err_result=CVLDAModel.predict(Xerr)==yerr;              
-                %             errcorr{bin+3}=[errcorr{bin+3};cv_err_result];
-                
-
+                %             errcorr{bin+3}=[errcorr{bin+3};cv_err_result];                
+            end
+   
+        end
     end
+    cvcorr{rpt}=cellfun(@(x)nnz(x)/length(x),cvresult);
+    shufcorr{rpt}=cellfun(@(x)nnz(x)/length(x),cvshufresult);
 end
 
 for bin=1:bins
-    for bin2=1:bins
-        [p_temp,~,~]=statistics.permutation(out.corr,out.shufcorr,1000);
-        [p_temp,~,~]=signrank(out.cvcorr{bin,bin2},out.shufcorr{bin,bin2});
-        if p_temp<0.001/(rpts*bins)
-            out.p(bin,bin2)=1;
-        else
-            out.p(bin,bin2)=0;
-        end
-    end
+     for bin2=1:bins
+         out.cvcorr{bin,bin2}=cellfun(@(x)x(bin,bin2),cvcorr);
+         out.shufcorr{bin,bin2}=cellfun(@(x)x(bin,bin2),shufcorr);
+     end
 end
+
+out.P=cellfun(@(x,y)ranksum(x,y),out.cvcorr,out.shufcorr);
+
+
+out.p_permutation=cellfun(@(x,y)statistics.permutationTest(x,y,100),out.cvcorr,out.shufcorr);
+% out.P=arrayfun(@(x)x<0.001/(rpts*bins),p_temp);
+
 
 end
--- Decoding/Decoding.m
+++ Decoding/Decoding.m
@@ -2,122 +2,131 @@ function out=Decoding(decdata,opt)
 %%%
 % Input:
 %     decdata is structure, including two structures (train and test, each including two 4-D matrix s1 and s2)
-%     decdata.train.s1 = (cellID * trlN * bins * rpts) matrix 
+%     decdata.train.s1 = (cellID * trlN * bins * rpts) matrix
 %%%
 arguments
     decdata (1,1) struct
     opt.decoder (1,:) char {mustBeMember(opt.decoder,{'SVM','LDA','NB'})} ='SVM';
 end
-    
+
 bins=size(decdata.train.s1,3);
 trlN=size(decdata.train.s1,2);
 rpts=size(decdata.train.s1,4);
 
-cv=cvpartition(trlN,'KFold',10);
-y=[zeros(trlN,1);ones(trlN,1)];
 
+y=[zeros(trlN,1);ones(trlN,1)];
 out=struct();
-cvcorr=cell(0);
-shufcorr=cell(0);
-%     errcorr{bin+3}=[];
-tic
+out.cvcorr=cell(1,bins);
+out.shufcorr=cell(1,bins);
+cv=cvpartition(trlN,'KFold',10);
+
 cv_trainingID=cell2mat(arrayfun(@(x)training(cv,x),[1:10],'UniformOutput',false));
 cv_testID=cell2mat(arrayfun(@(x)test(cv,x),[1:10],'UniformOutput',false));
 % s1kf=arrayfun(@(x)decdata.train.s1(:,x,1,1),cv_trainingID,'UniformOutput',false);
 % s1kf_1=reshape(s1kf(cv_trainingID),[],10);
-
-for rpt=1:rpts
-%     fprintf('rpts %d of %d\n',rpt,rpts);
-    for bin=1:bins
-        cvresult=[];
-        cvshufresult=[];        
-            for kf=1:cv.NumTestSets                
-                s1kf=decdata.train.s1(:,cv_trainingID(:,kf),bin,rpt);
-                s2kf=decdata.train.s2(:,cv_trainingID(:,kf),bin,rpt);
-                varsel=any(s1kf-s1kf(:,1),2) & any(s2kf-s2kf(:,1),2);
-                s1kf=s1kf(varsel,:);
-                s2kf=s2kf(varsel,:);
-                %             Xerr=[s1err(:,varsel);s2err(:,varsel)];
-                %             yerr=[zeros(size(s1err,1),1);ones(size(s2err,1),1)];
-                Xkf=cat(2,s1kf,s2kf)';
-                ykf=y([cv_trainingID(:,kf);cv_trainingID(:,kf)]);
-                
-                s1Tkf=decdata.test.s1(varsel,cv_testID(:,kf),bin,rpt);
-                s2Tkf=decdata.test.s2(varsel,cv_testID(:,kf),bin,rpt);
-                XTkf=cat(2,s1Tkf,s2Tkf)';
-                yTkf=y([cv_testID(:,kf);cv_testID(:,kf)]);
-                yshufTkf=yTkf(randperm(numel(yTkf)));
-                
-                
-                if strcmp(opt.decoder,'SVM')
-                    SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',true);
-                    modelPredict=SVMM.predict(XTkf);
-                elseif strcmp(opt.decoder,'LDA')
-                    LDAM=fitcdiscr(Xkf,ykf);
-                    modelPredict=LDAM.predict(XTkf);
-                elseif strcmp(opt.decoder,'NB')
-                    NBM=fitcnb(Xkf,ykf);
-                    modelPredict=NBM.predict(XTkf);
+switch(nnz(contains(fieldnames(decdata),'test')))
+    case 1 
+        for rpt=1:rpts
+            fprintf('rpts %d of %d\n',rpt,rpts);
+            for bin=1:bins                
+                for kf=1:cv.NumTestSets
+                    s1kf=decdata.train.s1(:,cv_trainingID(:,kf),bin,rpt);
+                    s2kf=decdata.train.s2(:,cv_trainingID(:,kf),bin,rpt);
+                    varsel=any(s1kf-s1kf(:,1),2) & any(s2kf-s2kf(:,1),2);
+                    s1kf=s1kf(varsel,:);
+                    s2kf=s2kf(varsel,:);                    
+                    Xkf=cat(2,s1kf,s2kf)';
+                    ykf=y([cv_trainingID(:,kf);cv_trainingID(:,kf)]);
+                    
+                    s1Tkf=decdata.test.s1(varsel,cv_testID(:,kf),bin,rpt);
+                    s2Tkf=decdata.test.s2(varsel,cv_testID(:,kf),bin,rpt);
+                    XTkf=cat(2,s1Tkf,s2Tkf)';
+                    yTkf=y([cv_testID(:,kf);cv_testID(:,kf)]);
+                    
+%                     yshufTkf=yTkf(randperm(numel(yTkf)));
+                    %             Xerr=[s1err(:,varsel);s2err(:,varsel)];
+                    %             yerr=[zeros(size(s1err,1),1);ones(size(s2err,1),1)];
+                    
+                    if strcmp(opt.decoder,'SVM')
+                        SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',true);
+                        modelPredict=SVMM.predict(XTkf);
+                    elseif strcmp(opt.decoder,'LDA')
+                        LDAM=fitcdiscr(Xkf,ykf);
+                        modelPredict=LDAM.predict(XTkf);
+                    elseif strcmp(opt.decoder,'NB')
+                        NBM=fitcnb(Xkf,ykf);
+                        modelPredict=NBM.predict(XTkf);
+                    end
+                    
+                    out.cvcorr{:,bin}=cat(1,out.cvcorr{:,bin},modelPredict==yTkf);
+                     for i=1:50
+                        yshufTkf=yTkf(randperm(numel(yTkf)));
+                        cvshufresult=modelPredict==yshufTkf;
+                        out.shufcorr{:,bin}=cat(1,out.shufcorr{:,bin},cvshufresult);
+                    end   
+%                     out.shufcorr{:,bin}=cat(1,out.shufcorr{:,bin},modelPredict==yshufTkf);
+                    %             cv_err_result=CVLDAModel.predict(Xerr)==yerr;
+                    
+                    
+                    %             errcorr{bin+3}=[errcorr{bin+3};cv_err_result];
                 end
                 
-                cvresult=cat(1,cvresult,modelPredict==yTkf);
-                cvshufresult=cat(1,cvshufresult,modelPredict==yshufTkf);
-                %             cv_err_result=CVLDAModel.predict(Xerr)==yerr;
-                
-                               
-                %             errcorr{bin+3}=[errcorr{bin+3};cv_err_result];
             end
-            cvcorr{rpt}{bin}=cvresult;
-            shufcorr{rpt}{bin}=cvshufresult; 
-    end
+        end
+    case 2   
+        out.cvcorr2=cell(1,bins);
+        for rpt=1:rpts
+            fprintf('rpts %d of %d\n',rpt,rpts);
+            for bin=1:bins                
+                for kf=1:cv.NumTestSets
+                    s1kf=decdata.train.s1(:,cv_trainingID(:,kf),bin,rpt);
+                    s2kf=decdata.train.s2(:,cv_trainingID(:,kf),bin,rpt);
+                    varsel=any(s1kf-s1kf(:,1),2) & any(s2kf-s2kf(:,1),2);
+                    s1kf=s1kf(varsel,:);
+                    s2kf=s2kf(varsel,:);                    
+                    Xkf=cat(2,s1kf,s2kf)';
+                    ykf=y([cv_trainingID(:,kf);cv_trainingID(:,kf)]);
+                    
+                    s1Tkf=decdata.test1.s1(varsel,cv_testID(:,kf),bin,rpt);
+                    s2Tkf=decdata.test1.s2(varsel,cv_testID(:,kf),bin,rpt);
+                    XTkf1=cat(2,s1Tkf,s2Tkf)';
+                    yTkf1=y([cv_testID(:,kf);cv_testID(:,kf)]);
+                    
+                    
+                    XTkf2=[decdata.test2.s1(varsel,:,bin,rpt),decdata.test2.s2(varsel,:,bin,rpt)]';
+                    yTkf2=[zeros(size(decdata.test2.s1,2),1);ones(size(decdata.test2.s1,2),1)];
+                    
+                    if strcmp(opt.decoder,'SVM')
+                        SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',true);
+                        modelPredict=SVMM.predict(XTkf1);
+                    elseif strcmp(opt.decoder,'LDA')
+                        LDAM=fitcdiscr(Xkf,ykf);
+                        modelPredict=LDAM.predict(XTkf1);
+                    elseif strcmp(opt.decoder,'NB')
+                        NBM=fitcnb(Xkf,ykf);
+                        modelPredict=NBM.predict(XTkf1);
+                    end
+                    
+                    out.cvcorr{:,bin}=cat(1,out.cvcorr{:,bin},modelPredict==yTkf1);
+                    out.cvcorr2{:,bin}=cat(1,out.cvcorr2{:,bin},SVMM.predict(XTkf2)==yTkf2);
+                    
+                    for i=1:200
+                        yshufTkf=yTkf1(randperm(numel(yTkf1)));
+                        cvshufresult=modelPredict==yshufTkf;
+                        out.shufcorr{:,bin}=cat(1,out.shufcorr{:,bin},cvshufresult);
+                    end                    
+                end               
+               
+            end
+        end
 end
-%
-% for rpt=1:rpts
-% %     fprintf('rpts %d of %d\n',rpt,rpts);
-%     for bin=1:bins
-%         cvresult=[];
-%         cvshufresult=[];
-%             for kf=1:cv.NumTestSets
-%                 s1kf=decdata.train.s1(:,cv_trainingID(:,kf),bin,rpt);
-%                 s2kf=decdata.train.s2(:,cv_trainingID(:,kf),bin,rpt);
-%                 varsel=any(s1kf-s1kf(:,1),2) & any(s2kf-s2kf(:,1),2);
-%                 s1kf=s1kf(varsel,:);
-%                 s2kf=s2kf(varsel,:);
-%                 %             Xerr=[s1err(:,varsel);s2err(:,varsel)];
-%                 %             yerr=[zeros(size(s1err,1),1);ones(size(s2err,1),1)];
-%                 Xkf=cat(2,s1kf,s2kf)';
-%                 ykf=y([cv_trainingID(:,kf);cv_trainingID(:,kf)]);
-%                 
-%                 s1Tkf=decdata.test.s1(varsel,cv_testID(:,kf),bin,rpt);
-%                 s2Tkf=decdata.test.s2(varsel,cv_testID(:,kf),bin,rpt);
-%                 XTkf=cat(2,s1Tkf,s2Tkf)';
-%                 yTkf=y([cv_testID(:,kf);cv_testID(:,kf)]);
-%                 yshufTkf=yTkf(randperm(numel(yTkf)));
-%                 
-%                 
-%                 if strcmp(opt.decoder,'SVM')
-%                     SVMM=fitcsvm(Xkf,ykf,'KernelFunction','linear','Standardize',true);
-%                     modelPredict=SVMM.predict(XTkf);
-%                 elseif strcmp(opt.decoder,'LDA')
-%                     LDAM=fitcdiscr(Xkf,ykf);
-%                     modelPredict=LDAM.predict(XTkf);
-%                 elseif strcmp(opt.decoder,'NB')
-%                     NBM=fitcnb(Xkf,ykf);
-%                     modelPredict=NBM.predict(XTkf);
-%                 end
-%                 
-%                 cvresult=cat(1,cvresult,modelPredict==yTkf);
-%                 cvshufresult=cat(1,cvshufresult,modelPredict==yshufTkf);
-%                 %             cv_err_result=CVLDAModel.predict(Xerr)==yerr;
-%                 
-%                                
-%                 %             errcorr{bin+3}=[errcorr{bin+3};cv_err_result];
-%             end
-%             cvcorr{rpt}{bin}=cvresult;
-%             shufcorr{rpt}{bin}=cvshufresult; 
-%     end
-% end
-toc
-out.cvcorr=cell2mat(cellfun(@cell2mat,cvcorr','UniformOutput',false));
-out.shufcorr=cell2mat(cellfun(@cell2mat,shufcorr','UniformOutput',false));
+for bin=1:bins  
+    [p_temp,~,~]=statistics.permutationTest(out.cvcorr{:,bin},out.shufcorr{:,bin},500);    
+    if p_temp<0.001/(rpts*bins)
+        out.p(:,bin)=1;
+    else
+        out.p(:,bin)=0;
+    end   
 end
+end
+
